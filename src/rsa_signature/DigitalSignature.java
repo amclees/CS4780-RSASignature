8f316fb06630036451cef99de0ac5af9d0a7fbe3
Andrew McLees <andrewmclees5@gmail.com>
2017-04-19 15:42:16 -0700

    Added signing and menu loop.

--- src/rsa_signature/DigitalSignature.java
+++ src/rsa_signature/DigitalSignature.java
@@ -1,5 +1,77 @@
 package rsa_signature;
 
-public class DigitalSignature {
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+public class DigitalSignature {
+  public static final int bufferSize = 1024;
+  
+  public static void writeSigned(File file, BigInteger[] publicKey) {
+    if(!file.isFile()) throw new IllegalArgumentException();
+    File signedFile = null;
+    try {
+      signedFile = new File(file.getCanonicalPath() + ".signed");
+      signedFile.createNewFile();
+    } catch(IOException e) {
+      if(KeyGen.noisy) System.out.println("Failed to make signed file path");
+    }
+    
+    MessageDigest messageDigest = null;
+    try {
+      messageDigest = MessageDigest.getInstance("MD5");
+    } catch (NoSuchAlgorithmException e) {
+      System.out.println("Your JVM is broken, MD5 is guaranteed to be an algorithm.");
+    }
+    
+    try {
+      InputStream fileInput = new BufferedInputStream(new FileInputStream(file));
+      for (int got = 1; got > 0;) {
+        int left = fileInput.available();
+        byte[] read = new byte[left < bufferSize ? left : bufferSize];
+        got = fileInput.read(read, 0, read.length);
+        messageDigest.update(read);
+      }
+      fileInput.close();
+    } catch(IOException e) {
+      if(KeyGen.noisy) System.out.println("Failed to get file bytes");
+    }
+    
+    byte[] digestBytes = messageDigest.digest();
+    BigInteger digest = new BigInteger(1, digestBytes);
+    
+    BigInteger signature = digest.modPow(publicKey[1], publicKey[0]);
+    
+    try {
+      ObjectOutputStream signedFileOut = new ObjectOutputStream(new FileOutputStream(signedFile));
+      signedFileOut.writeObject(signature);
+      
+      
+      InputStream fileInput = new BufferedInputStream(new FileInputStream(file));
+      for (int got = 1; got > 0;) {
+        int left = fileInput.available();
+        byte[] read = new byte[left < bufferSize ? left : bufferSize];
+        got = fileInput.read(read, 0, read.length);
+        signedFileOut.write(read);
+      }
+      fileInput.close();
+      
+      
+      signedFileOut.close();
+    } catch(IOException e) {
+      if(KeyGen.noisy) System.out.println("Failed to write signed file");
+    }
+  }
+  
+  public static boolean verify(File file, BigInteger[] privateKey) {
+    return false;
+  }
+  
 }
--- src/rsa_signature/Main.java
+++ src/rsa_signature/Main.java
@@ -1,5 +1,63 @@
 package rsa_signature;
 
-public class Main {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while(true) {
+      System.out.printf("Actions:%n  0 - Exit%n  1  \"Send\" files%n  2  \"Receive\" files%n");
+      int action = sc.nextInt();
+      if(action == 1) {
+        System.out.println("Enter a filename:");
+        String filename = sc.next();
+        DigitalSignature.writeSigned(new File(filename), getKeys(false));
+      } else if(action == 2) {
+        System.out.println("Enter a filename that ends in \".signed\":");
+        String filename = sc.next();
+        if(!filename.matches(".*" + Pattern.quote(".") + "signed")) {
+          System.out.println("That file is not valid (not a .signed)");
+          continue;
+        }
+        DigitalSignature.verify(new File(filename), getKeys(true));
+      } else {
+        break;
+      }
+    }
+  }
+  
+  public static BigInteger[] getKeys(boolean privateKey) {
+    File keyFile = new File(privateKey ? "privkey.rsa" : "pubkey.rsa");
+    ObjectInputStream keyIn = null;
+    
+    BigInteger n = null;
+    BigInteger exponent = null;
+    try {
+      keyIn = new ObjectInputStream(new FileInputStream(keyFile));
+      n = (BigInteger) keyIn.readObject();
+      exponent = (BigInteger) keyIn.readObject(); 
+      if(KeyGen.noisy) System.out.println("Read keys");
+    } catch(IOException e) {
+      if(KeyGen.noisy) System.out.println("Failed to read keys");
+    } catch(ClassCastException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } finally {
+      if(keyIn != null) {
+        try {
+          keyIn.close();
+        } catch(IOException e) {}
+      }
+    }
+    BigInteger[] keys = { n, exponent };
+    return keys;
+  }
 }
